4624b3bc9324a060fa82f52dc22ba90a6ddce70c
argonaultes <gael@argonaultes.fr>
2024-12-19 10:53:01 +0100

    set dependencies selenium

--- qlearning.py
+++ qlearning.py
@@ -1,23 +1,24 @@
+from selenium import webdriver
 
 # Définir une liste d'états, dans notre cas, une liste d'adresses cibles par exemple
-states = ...
+states = [ '/subscribe', '/']
 # Définir une liste d'actions possibles, dans notre cas, cliquer sur un bouton donné, ou cliquer sur un lien
 actions = ...
 # Pour chaque état obtenu, affecter une récompense. Si on cherche à atteindre une page en particulier, donner un nombre de points positifs, et pour les autres adresses, des points négatifs
 rewards = ...
 
 # Initialiser le dictionner avec uniquement des zéros
-# Dans ce dicitionnaire, pour chaque état, pour chaque action, un score est associé, ici tous les scores sont à zéro.
+# Dans ce dictionnaire, pour chaque état, pour chaque action, un score est associé, ici tous les scores sont à zéro.
 Q = 
 
 # Définir les hyperparamètres suivants
 alpha = 0.1 # taux d'apprentissage
 gamma = 0.9 # rapprocher gamma de 1 si nous avons beaucoup d'états, sinon, le rapprocher de 0
-epsilon = 0.2 # exploration probability
+epsilon = 0.2 # exploration probability entre 0 et 1
 num_episodes = 5000
 
 # Créer une fonction dont l'objectif est de choisir une action en suivant la stratégie epsilon-greedy
-# choisir une valeur aléatoire comprise entre 0 et epsilon
+# choisir une valeur aléatoire comprise entre 0 et 1
 # si < epsilon, se lancer dans l'exploration, c'est à dire à choisir une action au hasard
 # si >= epsilon, se lancer dans l'exploitation, c'est à dire choisir l'action avec le grand score dans la Q-Table pour l'état en cours
 # cette fonction renvoie donc une action
@@ -25,17 +26,16 @@ def choose_action(state):
     pass
 
 # créer une fonction qui va renvoyer l'état attendu pour une action donné
-# créer un comportement par défaut qui choist un état au hasard si l'action ne correspond à aucune prévue
+# créer un comportement par défaut qui choisit un état au hasard si l'action ne correspond à aucune prévue
 # renvoyer un tuple prochain_état, récompense
 def step(state, action):
     pass
 
 # Définir une fonction qui doit mettre à jour la Q-Table selon l'équation dite de Bellman
-# L'équation de Bellman permet de mettre à jour pour une action et un état donné en paramètre son nouveau suivant la formule : (1 - alpha) * Q_s-1 + alpha * (reward + gamma * max(Q_s))
+# L'équation de Bellman permet de mettre à jour pour une action et un état donné en paramètre son nouveau suivant la formule : (1 - alpha) * Q_(s-1) + alpha * (reward + gamma * max(Q_s))
 def update_Q(state, action, reward, next_state):
     pass
 
-# Run the Q-learning algorithm for a fixed number of episodes
 # Pour terminer, entrainer notre Q-Table sur un nombre fixe d'épisodes
 for ... :
     # Initialiser la navigation à un état aléatoire
